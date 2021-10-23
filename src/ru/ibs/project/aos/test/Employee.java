a2ffa1e5493d5a2258cb85e23415ed2519f9d87d
oalikin88 <67868065+oalikin88@users.noreply.github.com>
2021-10-23 19:50:28 +0300

    внесены исправления в класс Employee

--- src/ru/ibs/project/aos/test/Employee.java
+++ src/ru/ibs/project/aos/test/Employee.java
@@ -2,8 +2,24 @@ package ru.ibs.project.aos.test;
 
 abstract public class Employee {
 
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public String department() {
         return null;
